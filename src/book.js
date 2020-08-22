092ec3bd1ea261dcb878a773fcc4935b7db731b1
Jordon Smith <jordon00smith@gmail.com>
2020-08-22 13:36:21 -0600

    Finish book.js test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,54 @@
-function createTitle() {
-  
+function createTitle(title) {
+    var bookTitle = `The ${title}`;
+    return bookTitle;
 }
+
+
+function buildMainCharacter(name, age, pronouns) {
+    var bookCharacter = {
+        name: name,
+        age: age,
+        pronouns: pronouns,
+    }
+    return bookCharacter;
+}
+
+
+function saveReview(review, reviews) {
+    if(reviews.indexOf(review) === -1) {
+        reviews.push(review);
+    } 
+    return reviews;
+}
+
+
+function calculatePageCount(title) {
+    var bookPageCount = (title.length * 20);
+    return bookPageCount;
+}
+
+
+function writeBook(bookTitle, bookCharacter, genre) {
+    var book = {
+        title: bookTitle,
+        mainCharacter: bookCharacter,
+        pageCount: calculatePageCount(bookTitle),
+        genre: genre,
+    }
+    return book;
+}
+
+
+function editBook(book) {
+    return book.pageCount = (book.pageCount * .75);
+}
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
